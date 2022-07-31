0777c4d7c3dcaf362c285230e37aa6b51eadc2a7
SergioFau <sergio-faurhotmail.com>
2022-07-31 18:13:28 +0200

    cambios3

---
+++ backend/EcomenderApi/Startup.cs
@@ -0,0 +1,51 @@
+// Unused usings removed
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using EcomenderApi.Models;
+using EcomenderApi.Data;
+
+namespace EcomenderApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+            services.AddSwaggerGen({c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "AASergio", Version = "v2" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
